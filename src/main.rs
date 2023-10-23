554d38ce7d51098b622baa6e1811875cf3332b12
alfredosa <asuarezaceves@gmail.com>
2023-10-23 09:12:04 +0200

    Picking code apart, use Legion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod camera;
+mod components;
 mod map;
 mod map_builder;
-mod player;
+mod spawners;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -10,28 +12,38 @@ mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::player::*;
+    pub use crate::spawners::*;
+    pub use crate::systems::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
 }
 
 use prelude::*;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs: World,
+    resources: Resources,
+    systems: Schedule,
 }
 
 impl State {
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
 
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start),
+            ecs,
+            resources,
+            systems: build_scheduler(),
         }
     }
 }
@@ -42,9 +54,9 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+        self.resources.insert(ctx.key); // insert the key pressed into the resources (Available for all systems)
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error"); // render the draw buffer (The draw buffer is the console that we are drawing to)
     }
 }
 
---
+++ src/systems/player_input.rs
@@ -0,0 +1,36 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(Player)]
+#[write_component(Point)]
+pub fn player_input(ecs: &mut SubWorld,
+    #[resource] map: &Map, 
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] camera: &mut Camera,
+    ) {
+    if let Some(key) = key {
+        let delta = match key {
+            VirtualKeyCode::Left => Point::new(-1, 0),
+            VirtualKeyCode::Right => Point::new(1, 0),
+            VirtualKeyCode::Up => Point::new(0, -1),
+            VirtualKeyCode::Down => Point::new(0, 1),
+            VirtualKeyCode::Space => Point::new(0, 0),
+            _ => Point::new(0, 0),
+        };
+        if delta.x != 0 || delta.y != 0 {
+            // Get the player position from the world. (Query the world for the player position) &mut Point is a mutable reference to the player position.
+            // How the Query works: We are querying the world for all entities that have a Point component and a Player component.
+            let mut players = <&mut Point>::query().filter(component::<Player>());
+            // ecs in iter_mut is a mutable reference to the world. We are iterating over all the players in the world.
+            // iter mut is Legion syntax for iterating over a query result with param mutability.
+            players.iter_mut(ecs).for_each(|pos| { 
+                let destination = *pos + delta;
+                if map.can_enter_tile(destination) {
+                    *pos = destination;
+                    camera.on_player_move(destination);
+                }
+            });
+        }
+    }
+    
+}
